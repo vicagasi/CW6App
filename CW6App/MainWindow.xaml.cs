da75cd290aca7eab8308a3610c4308256e3ded68
Victor Gasior <vicagasi@ut.utm.edu>
2022-02-15 17:44:21 -0600

    Add project files.

---
+++ CW6App/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CW6App
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            OleDbConnection cn;
+            cn = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|\\CW6Database.accdb");
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+            /* === ASSETS === */
+            // Employee ID
+            string query = "select* from Assets";
+            AssetEmpID.Text = getData(query);
+            // Asset ID
+            query = "SELECT AssetID from Assets";
+            AssetAssetID.Text = getData(query);
+            // Asset Descriptions
+            query = "SELECT Description from Assets";
+            AssetDesc.Text = getData(query);
+        }
+
+        private void Button_ClickEMP(object sender, RoutedEventArgs e)
+        {
+
+            /* === EMPLOYEES === */
+            // Employee ID
+            String query = "select* from Employees";
+            EmpEmpID.Text = getData(query);
+            // Asset ID
+            query = "SELECT FirstName from Employees";
+            FirstName.Text = getData(query);
+            // Asset Descriptions
+            query = "SELECT LastName from Employees";
+            LastName.Text = getData(query);
+        }
+
+        public string getData(string query)
+        {
+            OleDbConnection cn;
+            cn = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|\\CW6Database.accdb");
+            OleDbCommand cmd = new OleDbCommand(query, cn);
+            cn.Open();
+            OleDbDataReader read = cmd.ExecuteReader();
+            string data = "";
+            while (read.Read())
+            {
+                data += read[0].ToString() + "\n";
+            }
+            return data;
+        }
+    }
+}
